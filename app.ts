18882df51a21c5c0a0d87cbb4000935a05ad74cc
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-08-17 16:58:01 +0530

    Classes and Abstract classes

--- app.ts
+++ app.ts
@@ -1,38 +1,61 @@
-/* Interfaces */
-// Interfaces is a programming structure/syntax that allows the computer to enforxe certain properties on an object(class)
+/* OOP : Classes and Abstract classes  */
+class Person {
+  // name: string;
+  // private name: string;
+  // By making a method private, you tell other programmers that this method is not intended to be accessed directly.
+  // By default its public and accessible.
+  protected name: string;
+  // Protected specifies access to class members in the member-list up to the next access specifier(public or private) or the end of the class definition
+  constructor(name: string) {
+    this.name = name;
+  }
+  greet() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
 
-interface Person {
-  name: string;
-  age: number;
-  height?: number;
-  // hello: () => void;
+  getName() {
+    return this.name;
+  }
+  setName(name: string) {
+    this.name = name;
+  }
 }
 
-const person: Person = {
-  name: "Naruto",
-  age: 21,
-  // hello: function () {
-  //   console.log(this.name + " says hi");
-  // },
-};
-// person.hello();
-
-// Interfaces allows us to interact with more complex objects and understand what properties they have.
-
-interface Employee extends Person {
-  eID: number;
+// Protected
+class Employee extends Person {
+  callMe() {
+    console.log(this.name);
+  }
 }
 
-const worker: Employee = {
-  name: "Itachi",
-  age: 21,
-  height: 175,
-  eID: 4,
-};
+const p1 = new Person("Naruto");
+// p1.name = "Itachi";
+// p1.getName();
 
-function getPerson(p: Person): Person {
-  return {
-    name: "Naruto",
-    age: 21,
-  };
+/* ABSTRACT CLASS */
+// Abstract class is a restricted class that cannot be used to create objects and designed to be specifically used as a base class.
+
+abstract class Animal {
+  abstract makeSound(duration: number): void;
+  move(duration: number) {
+    console.log("Moving along.......");
+    this.makeSound(duration);
+  }
 }
+// Abstract class is meant to act as a base class that will be inherited from by a subclass.
+
+class Dog extends Animal {
+  makeSound(duration: number) {
+    console.log("woof woof");
+  }
+}
+class Cat extends Animal {
+  makeSound(duration: number) {
+    console.log("meow meow");
+  }
+}
+
+const dog = new Dog();
+dog.move(10);
+
+// Any abstract method is typically gonna be utilized by a concrete implementation in the abstarct class
