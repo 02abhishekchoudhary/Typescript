1fd8f64884da41d88abbc6e699c70687e7ac553c
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-08-17 11:50:28 +0530

    Primitives Type

--- app.ts
+++ app.ts
@@ -1,10 +1,35 @@
-// Error
-// function test(x: number): number { }
+/* PRIMITIVES TYPES */
 
-// test("hello");
+// 1. Number
+// Explicit type
+let x: number = 2;
 
-function test(x: number): number {
-  return x + 1;
-}
+// Implicit type
+let y = 2;
 
-test(2);
+let z: number;
+z = 3;
+
+// 2. String
+let a: string;
+let b = 10;
+let c = "Pandas";
+a = `${b} ${c}`;
+
+// 3. Boolean
+let d: boolean;
+d = true;
+d = false;
+
+// Null : Use null when you want to explicitly define somethig as empty or non-existeent. Null will set type of any.
+let result = null;
+result = 2;
+result = "hello";
+
+// Undefined: is more used as a placeholder that means a variabe has been declared but has not yet been assigned a value.
+let result1 = undefined;
+result1 = "hello";
+result1 = 2;
+
+// Void: function returns nothing
+// Never : is used when we explicitly define thaat a function never returns
