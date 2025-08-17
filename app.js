804833296b26f75cfe69858896a2d0f7fff62a8a
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-08-17 11:35:59 +0530

    TS config

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Error
+// function test(x: number): number { }
+Object.defineProperty(exports, "__esModule", { value: true });
+// test("hello");
+function test(x) {
+    return x + 1;
+}
+console.log(test(2));
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,10 @@
+// Error
+// function test(x: number): number { }
+
+// test("hello");
+
+function test(x: number): number {
+  return x + 1;
+}
+
+test(2);
