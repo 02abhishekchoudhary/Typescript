9e46c6ac80d7cf7a7c336aef5d6763d982c761b3
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-08-17 16:25:42 +0530

    ADVANCE FUNCTION TYPES

--- app.ts
+++ app.ts
@@ -1,52 +1,22 @@
-/* FUNCTION TYPES */
-function add(x: number, y: number) {
-  if (x == 0) {
-    return "invalid";
+/* ADVANCE FUNCTION TYPES */
+
+// rest paramter
+function sum(...numbers: number[]) {}
+sum(1, 2, 3);
+sum();
+sum(1, 2, 3, 4, 5, 6);
+
+// Overloaded functions: is a function that has different call signatures and can accept different types
+function getItemLength(name: string): number;
+function getItemLength(name: string[]): number;
+function getItemLength(nameOrNames: unknown): number {
+  if (typeof nameOrNames === "string") {
+    return nameOrNames.length;
+  } else if (Array.isArray(nameOrNames)) {
+    return nameOrNames.length;
   }
-  return x + y;
+  return 0;
 }
 
-// OPtional parameter
-function makeName(firstName: string, lastName: string, middleName?: string) {
-  if (middleName) return firstName + " " + middleName + " " + lastName;
-  return firstName + " " + lastName;
-}
-const fullName = makeName("Naruto", "Uzumaki");
-
-// Default parameter
-function makeName1(firstName: string, lastName: string, middleName = "Kumar") {
-  if (middleName) return firstName + " " + middleName + " " + lastName;
-  return firstName + " " + lastName;
-}
-const fullName1 = makeName1("Naruto", "Uzumaki");
-
-// Function takess function as parameter
-
-function mul(x: number, y: number): number {
-  return x * y;
-}
-
-function div(x: number, y: number): number {
-  return x / y;
-}
-
-function applyFunc(
-  funcs: ((a: number, b: number) => number)[],
-  values: [number, number][]
-): number[] {
-  const results = [] as number[];
-  for (let i = 0; i < funcs.length; i++) {
-    const args = values[i];
-    const result = funcs[i](args[0], args[1]);
-    results.push(result);
-  }
-  return results;
-}
-
-applyFunc(
-  [mul, div],
-  [
-    [1, 2],
-    [4, 5],
-  ]
-);
+getItemLength("");
+getItemLength(["", ""]);
